93ea1325ff13af0eee2c4b22f8ced7266cc84346
valerijadrinek <valerijadrinek@gmail.com>
2023-10-25 20:11:14 +0200

    twig initialization

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/', name: 'question_')]
+#[Route('/', name: 'app_question_')]
 class QuestionController extends AbstractController {
 
     #[Route('/', name: 'list')]
