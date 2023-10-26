ab07086094f16911d1494ee135d4663005ed2335
valerijadrinek <valerijadrinek@gmail.com>
2023-10-26 14:19:06 +0200

    encore

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,15 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+
+import $ from 'jquery';
+
 /** simple code to deal with voting */
 var $container = $('.js-vote-arrows');
 $container.find('a').on('click', function(e) {
