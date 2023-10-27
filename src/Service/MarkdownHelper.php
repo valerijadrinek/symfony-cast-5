224fd2686b3ded4409021337a0def838081f24c7
valerijadrinek <valerijadrinek@gmail.com>
2023-10-27 19:04:36 +0200

    services added

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -4,19 +4,32 @@ namespace App\Service;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Contracts\Cache\CacheInterface;
+use Psr\Log\LoggerInterface;
 
 class MarkdownHelper 
 {
+    private $isDebug;
     private $markdownParser;
     private $cache;
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache)
+    private $logger;
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug, LoggerInterface $logger)
     {
+        $this->isDebug = $isDebug;
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
+        $this->logger = $logger;
+       
     }
     
     public function parse(string $source): string
     {
+        if (stripos($source, 'jar') !== false) {
+            $this->logger->info('Meow!');
+        }
+
+        if ($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
+        }
         
         return $this->cache->get('markdown_'.md5($source), function() use($source) {
             return $this->markdownParser->transformMarkdown($source);
